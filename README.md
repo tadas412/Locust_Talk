ad6cb4238f15451515b583b9bd735ba58a80e90a
Tadas Antanavicius <tadas412@gmail.com>
2014-12-11 01:28:12 -0500

    homepage template test

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ Web app that allows visitors to post messages on a message board.
 
 
 
-<h1> References </h1>
+<h2> References </h2>
 
 https://devcenter.heroku.com/articles/getting-started-with-python-o - Setting up Flask to work with Heroku
--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+import "views/homepage.py"
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-	return "Hello World!"
+def homepage():
+	return homepage_view()
 
 if __name__ == '__main__':
-	app.run()
+	app.run(debug=True)
