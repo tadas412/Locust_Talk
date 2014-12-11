5aed80ffc73c29a41191d741ab8136fb2841abce
Tadas Antanavicius <tadas412@gmail.com>
2014-12-11 03:16:22 -0500

    working linkage with fake categories

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
-import "views/homepage.py"
+from flask import render_template
+import sys, os
+curdir = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(curdir + "/views")
+import homepage as hp
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-	return homepage_view()
+	return render_template('homepage.html', l=hp.get_categories())
 
 if __name__ == '__main__':
 	app.run(debug=True)
